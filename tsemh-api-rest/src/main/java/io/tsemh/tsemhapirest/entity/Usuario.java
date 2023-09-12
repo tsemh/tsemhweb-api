7f464d111539f4ce2e0786cfff332afe1ae6e9b3
Tiago  Silva <44248308+tsemh@users.noreply.github.com>
2023-09-12 09:54:12 -0300

    long to int

--- tsemh-api-rest/src/main/java/io/tsemh/tsemhapirest/entity/Usuario.java
+++ tsemh-api-rest/src/main/java/io/tsemh/tsemhapirest/entity/Usuario.java
@@ -68,11 +68,11 @@ public class Usuario implements UserDetails{
 		this.descricao = descricao;
 	}
 
-	public long getId() {
+	public Int getId() {
         return id;
     }
 
-    public void setId(long id) {
+    public void setId(Int id) {
         this.id = id;
     }
 
