58306dc4e9413920b226f5a80c45345a4937dc10
Tiago  Silva <44248308+tsemh@users.noreply.github.com>
2023-09-13 08:48:02 -0300

    allowedOrigins

--- tsemh-api-rest/src/main/java/io/tsemh/tsemhapirest/config/CorsConfig.java
+++ tsemh-api-rest/src/main/java/io/tsemh/tsemhapirest/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         		.allowCredentials(true)
-                .allowedOrigins("/**")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
